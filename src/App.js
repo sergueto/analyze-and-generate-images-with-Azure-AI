0f20e7e9b1b2354d52042b9ebc58ac63381ceeda
Sergio <sergiobastidaslosada@gmail.com>
2023-11-30 20:48:54 -0500

    estructura basica web

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const [imageUrl, setImageUrl] = useState('');
+
+  return (
+    <div>
+      <h1>Título</h1>
+      <input 
+        type="text" 
+        value={imageUrl} 
+        onChange={(e) => setImageUrl(e.target.value)} 
+        placeholder="Ingrese la URL de la imagen"
+      />
+      <button onClick={() => console.log('Análisis de imagen')}>Analizar Imagen</button>
+      <button onClick={() => console.log('Generación de imagen')}>Generar Imagen</button>
+    </div>
+  );
 }
 
 export default App;
