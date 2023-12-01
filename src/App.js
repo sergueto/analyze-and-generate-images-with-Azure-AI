0c5cdf6dd7380ce8f91d915c6072b9d8c248fed3
Sergio <sergiobastidaslosada@gmail.com>
2023-11-30 21:39:48 -0500

    modifiy web

--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-function App() {
-  const [imageUrl, setImageUrl] = useState('');
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      url: '',
+    };
+  }
 
-  return (
-    <div>
-      <h1>Título</h1>
-      <input 
-        type="text" 
-        value={imageUrl} 
-        onChange={(e) => setImageUrl(e.target.value)} 
-        placeholder="Ingrese la URL de la imagen"
-      />
-      <button onClick={() => console.log('Análisis de imagen')}>Analizar Imagen</button>
-      <button onClick={() => console.log('Generación de imagen')}>Generar Imagen</button>
-    </div>
-  );
+  analizarImagen() {
+    const url = this.state.url;
+    // Aquí va el código para analizar la imagen
+  }
+
+  generarImagen() {
+    // Aquí va el código para generar la imagen
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>GUI Simple</h1>
+        <input
+          type="text"
+          id="url"
+          placeholder="Introduce la URL de la imagen"
+          value={this.state.url}
+          onChange={(event) => this.setState({ url: event.target.value })}
+        />
+        <button onClick={() => this.analizarImagen()}>Analizar Imagen</button>
+        <button onClick={() => this.generarImagen()}>Generar Imagen</button>
+      </div>
+    );
+  }
 }
 
 export default App;
